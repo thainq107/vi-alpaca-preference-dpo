24fce55e8531a3a1b545906c455d32a1d26a3ebc
ThaiNQ <110602193+thainq107@users.noreply.github.com>
2025-04-27 10:23:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ generator = pipeline("text-generation", model="thainq107/Llama-3.2-1B-Instruct-d
 
 def main():
     st.title('Vietnamese Custom Preference Model')
-    st.header('Model: LLaMA-3.2-1B. Dataset: Alpaca-Vi)
+    st.header('Model: LLaMA-3.2-1B. Dataset: Alpaca-Vi')
     text_input = st.text_input("Sentence: ", "Hãy giải thích về trí tuệ nhân tạo bằng tiếng Việt")
     output = generator([{"role": "user", "content": question}], max_new_tokens=128)[0]
     st.success(output)
